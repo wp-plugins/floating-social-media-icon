6723f176095cf9791d96ea1af759e72045b873c0
Acurax <Acurax@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-04 18:33:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/floating-social-media-icon/trunk@581804 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-help.php
+++ social-help.php
@@ -1,6 +1,6 @@
 <div id="acx_help_page">
 <div id="acx_fsmi_premium">
-<a style="margin: 8px 0px 0px 10px; float: left; font-size: 16px; font-weight: bold;" href="admin.php?page=Acurax-Social-Icons-Premium">Fully Featured - Premium Floating Social Media Icon</a>
+<a style="margin: 8px 0px 0px 10px; float: left; font-size: 16px; font-weight: bold;" href="http://clients.acurax.com/floating-socialmedia.php" target="_blank">Fully Featured - Premium Floating Social Media Icon</a>
 <a style="margin: -14px 0px 0px 10px; float: left;" href="http://clients.acurax.com/floating-socialmedia.php?utm_source=plugin_settings_top&utm_medium=link&utm_campaign=plugin_settings" target="_blank"><img src="<?php echo plugins_url('images/yellow.png', __FILE__);?>"></a>
 </div> <!-- acx_fsmi_premium -->
 <h2>Floating Social Media Icon - Wordpress Plugin - Help/Support</h2>
